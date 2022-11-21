9ca051e2a09cc8b17063afb2fae47dd6c59559e2
Brian Lopez <brianlama1220@gmail.com>
2022-11-21 19:49:15 -0400

    update login

--- src/components/LoginComponent.js
+++ src/components/LoginComponent.js
@@ -1,31 +1,98 @@
-import { View, TextInput, StyleSheet, TouchableOpacity, } from "react-native";
-import { useState } from "react";
-const LoginComponent = () => {
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
-    return(
-        <View>
-            <View>
-                <TextInput
-                    autoCapitalize="none"
-                    autoCorrect={false}
-                    placeholder="email"
-                    onChangeText={(email) => setEmail(email)}
-                />
-            </View>
-            <View>
-                <TextInput
-                    autoCapitalize="none"
-                    autoCorrect={false}
-                    placeholder="Password"
-                    secureTextEntry={true}
-                    onChangeText={(password) => setPassword(password)}
-                />
-            </View>
-            <TouchableOpacity>
-                <Text>LOGIN</Text>
-            </TouchableOpacity>
-        </View>
-    )
+import { StatusBar } from "expo-status-bar";
+import React, { useState } from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  TextInput,
+  Button,
+  TouchableOpacity,
+} from "react-native";
+
+export default function LoginComponent({toRegister}) {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  return (
+    <View style={styles.container}>
+      <Image style={styles.image} source={require("../images/app-logo.png")} />
+
+      <StatusBar style="auto" />
+      <View style={styles.inputView}>
+        <TextInput
+          style={styles.TextInputEmail}
+          placeholder="Email"
+          placeholderTextColor="#003f5c"
+          onChangeText={(email) => setEmail(email)}
+        />
+      </View>
+
+      <View style={styles.inputView}>
+        <TextInput
+          style={styles.TextInputPassword}
+          placeholder="Password"
+          placeholderTextColor="#003f5c"
+          secureTextEntry={true}
+          onChangeText={(password) => setPassword(password)}
+        >
+        </TextInput>
+      </View>
+
+      <TouchableOpacity onPress={toRegister}>
+        <Text style={styles.forgot_button}>Create an account</Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity style={styles.loginBtn}>
+        <Text style={styles.loginText}>LOGIN</Text>
+      </TouchableOpacity>
+    </View>
+  );
 }
-export default LoginComponent;
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+
+  image: {
+    width: 280,
+    height: 250,
+  },
+
+  inputView: {
+    backgroundColor: "grey",
+    borderRadius: 30,
+    width: "70%",
+    height: 45,
+    marginBottom: 20,
+
+    alignItems: "center",
+  },
+
+  TextInputPassword: {
+    height: 50,
+    padding: 10,
+  },
+  TextInputEmail: {
+    height: 50,
+    padding: 10,
+  },
+
+  forgot_button: {
+    height: 30,
+    marginBottom: 30,
+  },
+
+  loginBtn: {
+    width: "80%",
+    borderRadius: 25,
+    height: 50,
+    alignItems: "center",
+    justifyContent: "center",
+    marginTop: 40,
+    backgroundColor: "#99ccff",
+  },
+});
--- src/screens/AuthenticationScreen.js
+++ src/screens/AuthenticationScreen.js
@@ -1,27 +1,18 @@
 import { useState } from "react";
 import LoginComponent from "../components/LoginComponent";
 import RegisterComponent from "../components/RegisterComponent";
-import { View, TouchableOpacity, Text } from "react-native";
+import { View, ScrollView, TouchableOpacity, Text } from "react-native";
 
 const LOGIN_SCREEN = 0;
 const REGISTER_SCREEN = 1;
 const AuthenticationScreen = () => {
   const [screen, setScreen] = useState(LOGIN_SCREEN);
   return (
-    <View>
-      {screen === LOGIN_SCREEN && <LoginComponent />}
+    <ScrollView>
+      {screen === LOGIN_SCREEN && <LoginComponent toRegister={() => {setScreen(REGISTER_SCREEN)}} />}
       {screen === REGISTER_SCREEN && <RegisterComponent />}
 
       <View>
-        {screen === LOGIN_SCREEN && (
-          <TouchableOpacity
-            onPress={() => {
-              setScreen(REGISTER_SCREEN);
-            }}
-          >
-            {/* <Text>Registrate</Text> */}
-          </TouchableOpacity>
-        )}
         {screen === REGISTER_SCREEN && (
           <TouchableOpacity
             onPress={() => {
@@ -33,7 +24,7 @@ const AuthenticationScreen = () => {
           </TouchableOpacity>
         )}
       </View>
-    </View>
+    </ScrollView>
   );
 };
 
